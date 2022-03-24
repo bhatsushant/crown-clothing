982391b81ae62ecdb3cb3767c9ac1ad33a99d14c
Sushant <sush1393@gmail.com>
2022-03-24 15:17:28 -0400

    add routing to our project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import Directory from "./components/Directory/Directory.component"
-import categories from './categories';
+import Navigation from "./components/routes/Navigation/Navigation.component";
+import Home from "./components/routes/home/Home.component";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Directory categories={categories}/>
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
---
+++ src/components/routes/home/Home.component.jsx
@@ -0,0 +1,8 @@
+import Directory from "../../Directory/Directory.component";
+import categories from '../../../categories';
+
+const Home = () => {
+    return <Directory categories={categories}/>
+}
+
+export default Home;
